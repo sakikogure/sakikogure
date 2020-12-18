051186cd4fd4103a90dcea9d678349137355c00b
sakikogure <zm18474@josai.ac.jp>
2020-12-18 01:55:51 +0000

    1218

--- ex6-5.js
+++ ex6-5.js
@@ -4,6 +4,6 @@ function taro65() {who65 = '太郎';}
 function hanako65() {who65 = '花子';}
 
 var who66 = 'world';
-function sayhello65() {alert('Hello, ' + who66 + '!');}
-function taro65() {who66 = '太郎';alert(who66+'が来た');}
-function hanako65() {who66 = '花子';alert(who66+'が来た');}
+function sayhello66() {alert('Hello, ' + who66 + '!');}
+function taro66() {who66 = '太郎';alert(who66+'が来た');}
+function hanako66() {who66 = '花子';alert(who66+'が来た');}
